50afa075600c425f79e30fe31e95664dd07d718c
you <you@gmail.com>
2022-01-26 17:31:49 +0600

    Initial commit

---
+++ venu/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ENGR SM SAMRAT\Desktop\eshopdjango-master\venu\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
